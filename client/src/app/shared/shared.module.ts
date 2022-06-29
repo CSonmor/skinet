6b393091cb2401365af4da114566cf69fc590f8e
CSonmor <CSonmor@users.noreply.github.com>
2022-06-29 11:53:01 -0600

    End of section 9

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagerComponent } from './components/pager/pager.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+
+
+
+@NgModule({
+  declarations: [
+    PagerComponent,
+    PagingHeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot()
+  ],
+  exports: [
+    PagerComponent,
+    PaginationModule,
+    PagingHeaderComponent
+  ]
+})
+export class SharedModule { }
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,107 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
+import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/product-type';
+import { ShopParams } from '../shared/models/shop-params';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss'],
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search', {static: true}) searchTerm: ElementRef;
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'priceDesc' },
+  ];
+  shopParams = new ShopParams();
+  productCount = 50;
+
+  constructor(private shopService: ShopService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService
+      .getProducts(this.shopParams)
+      .subscribe(
+        (rsp) => {
+          this.products = rsp.data;
+          this.shopParams.pageNumber = rsp.pageIndex;
+          this.shopParams.pageSize = rsp.pageSize;
+          this.productCount = rsp.count;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe(
+      (rsp) => {
+        this.brands = [{ id: 0, name: 'All' }, ...rsp];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe(
+      (rsp) => {
+        this.types = [{ id: 0, name: 'All' }, ...rsp];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onBrandSelected(brandId: number) {
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string) {
+    this.shopParams.sort = sort;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any) {
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}

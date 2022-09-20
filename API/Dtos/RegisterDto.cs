406322f3ed1c04df689265a63f10a7465d056d1c
CSonmor <CSonmor@users.noreply.github.com>
2022-09-20 10:54:01 -0600

    End of section 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,19 @@
-﻿namespace API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+            ErrorMessage = "Password must have at least: 1 uppercase, 1 lowercase, 1 digit, and 1 special character. It must be at least 6 characters long.")]
         public string Password { get; set; }
     }
 }
